d6e771d2cc27919d52e26740964f70107776bb5d
Ctimene <nelliegibb31@gmail.com>
2025-07-03 21:39:00 -0400

    Game Update

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ window_width = 800
 window_height = 600
 
 window = pygame.display.set_mode((window_width, window_height))
-window.fill((0, 0, 0))
 pygame.display.set_caption("My First Pygame")
 
 player = pygame.Rect((300, 250, 50, 50))
@@ -16,7 +15,7 @@ while run:
 
     window.fill((0, 0, 0,))
 
-    pygame.draw.rect(window, (255, 0, 0), player)
+    pygame.draw.rect(window, (255, 255, 255), player)
 
     key = pygame.key.get_pressed()
     if key[pygame.K_a] == True:
@@ -27,17 +26,19 @@ while run:
         player.move_ip(0, -2)
     elif key[pygame.K_s] == True:
         player.move_ip(0, 2)
-    elif key[pygame.K_c] == True:
-        if window.fill == (0, 0, 0):
-            window.fill((255, 255, 255))
-            pygame.draw.rect(window, (0, 255, 0), player)
-        else:
-            window.fill((0, 0, 0))
-            pygame.draw.rect(window, (255, 0, 0), player)
+    elif key[pygame.K_SPACE] == True:
+        window.fill((255, 255, 255))
+        pygame.draw.rect(window, (0, 0, 0), player)
+    elif key[pygame.K_r] == True:
+        pygame.draw.rect(window, (255, 0, 0), player)
+    elif key[pygame.K_b] == True:
+        pygame.draw.rect(window, (0, 0, 255), player)
+    elif key[pygame.K_g] == True:
+        pygame.draw.rect(window, (0, 255, 0), player)
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
         
     pygame.display.update()
 
