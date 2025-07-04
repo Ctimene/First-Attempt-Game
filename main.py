605b8b3eb77cf3d716ed33cb4f4eca040cd6c8d6
Ctimene <nelliegibb31@gmail.com>
2025-07-03 20:19:34 -0400

    Game Update

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import pygame
 
 pygame.init()
 
-window_size = window_width, window_height = 800, 600
+window_width = 800
+window_height = 600
+
+window = pygame.display.set_mode((window_width, window_height))
+window.fill((0, 0, 0))
 pygame.display.set_caption("My First Pygame")
 
 player = pygame.Rect((300, 250, 50, 50))
@@ -10,9 +14,9 @@ player = pygame.Rect((300, 250, 50, 50))
 run = True
 while run:
 
-    window_size.fill((0, 0, 0,))
+    window.fill((0, 0, 0,))
 
-    pygame.draw.rect(window_size, (255, 0, 0), player)
+    pygame.draw.rect(window, (255, 0, 0), player)
 
     key = pygame.key.get_pressed()
     if key[pygame.K_a] == True:
@@ -24,12 +28,12 @@ while run:
     elif key[pygame.K_s] == True:
         player.move_ip(0, 2)
     elif key[pygame.K_c] == True:
-        if window_size.fill == (0, 0, 0):
-            window_size.fill((255, 255, 255))
-            pygame.draw.rect(window_size, (0, 255, 0), player)
+        if window.fill == (0, 0, 0):
+            window.fill((255, 255, 255))
+            pygame.draw.rect(window, (0, 255, 0), player)
         else:
-            window_size.fill((0, 0, 0))
-            pygame.draw.rect(window_size, (255, 0, 0), player)
+            window.fill((0, 0, 0))
+            pygame.draw.rect(window, (255, 0, 0), player)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
